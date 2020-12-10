1ca8c88cda9d8d7f285b3573747a25d50fbcf69d
Alex Rodriguez <60060421+elysianlouie@users.noreply.github.com>
2020-12-10 02:05:57 -0600

    Created main.py outlining steps to be followed

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# probability [P] array for a single throw
+
+# P array for up to 3 throws (do store P of going to jail)
+
+# P matrix
+# redistribution of P from "chance", "community chest" and "go to jail" tiles
+
+# diagonalization of P matrix
+# self-multiplying until change is minimal (must define a threshold)
+# de-diagonalization
+
+# extra: analyze profitability taking into account cost, rent and probability
