ce6038d6fedf379372da1f8d5908814db65a5727
Alex Rodriguez <60060421+elysianlouie@users.noreply.github.com>
2020-12-10 02:26:26 -0600

    Declared P array for single two-dice throw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-# probability [P] array for a single throw
+# probability (P) array for a single two-dice throw where 0-th element is the probability of getting doubles
+throw1 = (1/36) * np.ndarray([6, 0, 0, 2, 2, 4, 4, 6, 4, 4, 2, 2, 0])
 
 # P array for up to 3 throws (do store P of going to jail)
 
