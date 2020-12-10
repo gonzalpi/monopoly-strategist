f9102965b9409839983e2c915db1c730f4b05046
Alex Rodriguez <60060421+elysianlouie@users.noreply.github.com>
2020-12-10 14:43:10 -0600

    Declared P vectors for up to 3 throws

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import numpy as np
 
-# probability (P) array for a single two-dice throw where 0-th element is the probability of getting doubles
-throw1 = (1/36) * np.ndarray([6, 0, 0, 2, 2, 4, 4, 6, 4, 4, 2, 2, 0])
+# probability (P) array for a single two-dice throw
+throw1          = (1/36)*np.array([0, 0, 2, 2, 4, 4, 6, 4, 4, 2, 2, 0])
 
-# P array for up to 3 throws (do store P of going to jail)
+# P array for up to 2 throws
+throw2          = np.zeros(24)
+throw2[0:12]    = throw1
+for i in range(6):
+    throw2[(2 + 2*i):(14 + 2*i)] = throw2[(2 + 2*i):(14 + 2*i)] + (1/36)*throw1
+
+# P array for up to 3 throws and P of throwing doubles 3 times in a row
+throw3          = np.zeros(36)
+throw3[0:12]    = throw1
+for j in range(6):
+    throw3[(2 + 2*i):(26 + 2*i)] = throw3[(2 + 2*i):(26 + 2*i)] + (1/36)*throw2
+doubles        = (1/6**3)
 
 # P matrix
 # redistribution of P from "chance", "community chest" and "go to jail" tiles
