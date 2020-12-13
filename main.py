caa7c8da0bb1f774e2c08fd768aa59c7545edfe1
Alex Rodriguez <60060421+elysianlouie@users.noreply.github.com>
2020-12-13 04:19:33 -0600

    P matrix ready
    Redistributed P from triple doubles, "chance", "community chest" and "go to jail" tiles.

--- main.py
+++ main.py
@@ -19,17 +19,64 @@ doubles         = (1/6)**3
 # P matrix
 P               = np.zeros((40,40))
 for k in range (40):
-    if k <= 3:  # when no split is needed to store throw3 in P
+    if k <= 3:                      # when no split is needed to store throw3 in P
         P[k,(1 + k):(37 + k)]   = throw3
-    else:       # when throw3 must be split to be stored in P
+    else:                           # when throw3 must be split to be stored in P
         P[k,0:(k - 3)]          = throw3[(39 - k):36]
         P[k,(1 + k):40]         = throw3[0:(39 - k)]
-    P[k,10]     = P[k,10] + doubles # probability of going to jail after triple doubles
-    
-# redistribution of P from "chance", "community chest" and "go to jail" tiles
+
+# P redistribution from triple doubles
+P[:,10]     = P[:,10] + doubles
+
+# P redistribution from "chance" cards
+P_temp      = (1/16)*(P[:,7] + P[:,22] + P[:,36])
+P[:,0]      = P[:,0]  + P_temp          # go
+P[:,5]      = P[:,5]  + P_temp          # reading railroad
+P[:,10]     = P[:,10] + P_temp          # jail
+P[:,11]     = P[:,11] + P_temp          # st. charles place
+P[:,24]     = P[:,24] + P_temp          # illinois avenue
+P[:,39]     = P[:,39] + P_temp          # boardwalk
+P[:,4]      = P[:,4]   + (1/16)*P[:,7]  # 3 tiles backwards
+P[:,19]     = P[:,19]  + (1/16)*P[:,22]
+P[:,33]     = P[:,33]  + (1/16)*P[:,36]
+P[:,12]     = P[:,12]  + (1/16)*P[:,7]  # nearest utility
+P[:,28]     = P[:,28]  + (1/16)*P[:,22]
+P[:,12]     = P[:,12]  + (1/16)*P[:,36]
+P[:,15]     = P[:,15]  + (1/16)*P[:,7]  # nearest railroad
+P[:,25]     = P[:,25]  + (1/16)*P[:,22]
+P[:,5]      = P[:,5]   + (1/16)*P[:,36]
+P[:,7]      = (7/16)*P[:,7]             # compensate P
+P[:,22]     = (7/16)*P[:,22]
+P[:,36]     = (7/16)*P[:,36]
+
+# P redistribution from "community chest" cards
+P_temp = (1/17)*(P[:,2] + P[:,17] +  P[:,33])
+P[:,0]      = P[:,0]  + P_temp  # go
+P[:,2]      = (16/17)*P[:,2]    # compensate P
+P[:,17]     = (16/17)*P[:,17]
+P[:,33]     = (16/17)*P[:,33]
+
+# P redistribution from landing in "go to jail" tile
+P[:,10]     = P[:,10] + P[:,30]
+P[:,30]     = 0
+
 
 # diagonalization of P matrix
 # self-multiplying until change is minimal (must define a threshold)
 # de-diagonalization
 
 # extra: analyze profitability taking into account cost, rent and probability
+
+'''
+np.set_printoptions(threshold=np.inf, precision=3, suppress=True) # prints whole array, 3 decimal places, no scientific notation
+print("Part 1")
+print(P[:,0:10])
+print("Part 2")
+print(P[:,10:20])
+print("Part 3")
+print(P[:,20:30])
+print("Part 4")
+print(P[:,30:40])
+for count in range(40):
+    print("tile: " + str(count) + " P: " + str(np.sum(P[count])))
+'''
