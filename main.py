0cb8b9ab194d32973a5230fe3cc9112df7c5737b
Alex Rodriguez <60060421+elysianlouie@users.noreply.github.com>
2020-12-13 05:01:56 -0600

    Made the code more readable
    Added blank lines, comments, removed test code at the end, and wrote a note and further work that could be done at the end.

--- main.py
+++ main.py
@@ -6,22 +6,25 @@ throw1          = (1/36)*np.array([0, 0, 2, 2, 4, 4, 6, 4, 4, 2, 2, 0])
 # P array for up to 2 throws (landing on 1:24) excluding doubles
 throw2          = np.zeros(24)
 throw2[0:12]    = throw1
+
 for i in range(6):
     throw2[(2 + 2*i):(14 + 2*i)] = throw2[(2 + 2*i):(14 + 2*i)] + (1/36)*throw1
 
 # P array for up to 3 throws and P of throwing doubles 3 times in a row (landing on 1:36)
 throw3          = np.zeros(36)
 throw3[0:12]    = throw1
+
 for j in range(6):
     throw3[(2 + 2*i):(26 + 2*i)] = throw3[(2 + 2*i):(26 + 2*i)] + (1/36)*throw2
 doubles         = (1/6)**3
 
 # P matrix
 P               = np.zeros((40,40))
+
 for k in range (40):
-    if k <= 3:                      # when no split is needed to store throw3 in P
+    if k <= 3:                          # when no split is needed to store throw3 in P
         P[k,(1 + k):(37 + k)]   = throw3
-    else:                           # when throw3 must be split to be stored in P
+    else:                               # when throw3 must be split to be stored in P
         P[k,0:(k - 3)]          = throw3[(39 - k):36]
         P[k,(1 + k):40]         = throw3[0:(39 - k)]
 
@@ -36,15 +39,19 @@ P[:,10]     = P[:,10] + P_temp          # jail
 P[:,11]     = P[:,11] + P_temp          # st. charles place
 P[:,24]     = P[:,24] + P_temp          # illinois avenue
 P[:,39]     = P[:,39] + P_temp          # boardwalk
+
 P[:,4]      = P[:,4]   + (1/16)*P[:,7]  # 3 tiles backwards
 P[:,19]     = P[:,19]  + (1/16)*P[:,22]
 P[:,33]     = P[:,33]  + (1/16)*P[:,36]
+
 P[:,12]     = P[:,12]  + (1/16)*P[:,7]  # nearest utility
 P[:,28]     = P[:,28]  + (1/16)*P[:,22]
 P[:,12]     = P[:,12]  + (1/16)*P[:,36]
+
 P[:,15]     = P[:,15]  + (1/16)*P[:,7]  # nearest railroad
 P[:,25]     = P[:,25]  + (1/16)*P[:,22]
 P[:,5]      = P[:,5]   + (1/16)*P[:,36]
+
 P[:,7]      = (7/16)*P[:,7]             # compensate P
 P[:,22]     = (7/16)*P[:,22]
 P[:,36]     = (7/16)*P[:,36]
@@ -52,6 +59,7 @@ P[:,36]     = (7/16)*P[:,36]
 # P redistribution from "community chest" cards
 P_temp = (1/17)*(P[:,2] + P[:,17] +  P[:,33])
 P[:,0]      = P[:,0]  + P_temp  # go
+
 P[:,2]      = (16/17)*P[:,2]    # compensate P
 P[:,17]     = (16/17)*P[:,17]
 P[:,33]     = (16/17)*P[:,33]
@@ -60,9 +68,11 @@ P[:,33]     = (16/17)*P[:,33]
 P[:,10]     = P[:,10] + P[:,30]
 P[:,30]     = 0
 
-# matrix power and P display
+# matrix power
 prob = np.linalg.matrix_power(P,100)
-np.set_printoptions(threshold=np.inf, precision=2, suppress=True)
+
+# display
+np.set_printoptions(threshold=np.inf, precision=2, suppress=True) # prints whole array, 3 decimal places, no scientific notation
 print("Probability percentage of landing on any given Monopoly tile.\nBegins with \"Go\" tile.")
 print("\nTiles 1:10")
 print(100*prob[0,0:10])
@@ -73,25 +83,5 @@ print(100*prob[0,20:30])
 print("\nTiles 31:40")
 print(100*prob[0,30:40])
 
-# diagonalization of P matrix
-# self-multiplying until change is minimal (must define a threshold)
-# de-diagonalization
-
-# extra: analyze profitability taking into account cost, rent and probability
-
-'''
-np.set_printoptions(threshold=np.inf, precision=3, suppress=True) # prints whole array, 3 decimal places, no scientific notation
-
-'''
-'''
-print("Part 1")
-print(P[:,0:10])
-print("Part 2")
-print(P[:,10:20])
-print("Part 3")
-print(P[:,20:30])
-print("Part 4")
-print(P[:,30:40])
-for count in range(40):
-    print("tile: " + str(count) + " P: " + str(np.sum(P[count])))
-'''
+# note: could be made more efficient by using eigvec*eigval^100*eigvec^-1
+# further work: analyze profitability taking into account price and rent too
